3979e47e67f2f01d3bbb48598d0778ea70be6880
Mohamed Aarish M <mmohamedaarish@kaartech.com>
2024-02-13 17:00:49 +0000

    changes

--- uatpayrollreportui/webapp/controller/View1.controller.js
+++ uatpayrollreportui/webapp/controller/View1.controller.js
@@ -1226,7 +1226,7 @@ sap.ui.define([
                 }
                 var oRecord = {
                     "AppName": appName, "MinDate": this.oFromDate, "MaxDate": this.oToDate, "Date": "", "PayCode": "", "CostCenter": "", "Activity": "",
-                    "WorkOrder": "", "Job": "", "Section": "", "Phase": "", "TotalHours": "", "ManagerApprovalName": this.loginName, "SaveSubmitStatus": "",
+                    "WorkOrder": "", "Job": "", "Section": "", "Phase": "", "TotalHours": "", "ManagerApprovalName": this.loginName, "SaveSubmitStatus": "Saved",
                     "PayrollApprovalStatus": "", "ManagerApprovalStatus": "", "NewRecord": true, "LocationCode": this.Location, "PersonnelSubArea": this.PersonalSubArea
                 };
                 aTimePeriodModel.timesheetData.push(oRecord);
